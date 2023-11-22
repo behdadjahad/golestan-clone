9e39689466a501fb0a9998a9ac237844563065a5
Sajad <imsajtj@gmail.com>
2023-11-22 23:50:33 +0330

    swagger added

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_spectacular.views import SpectacularJSONAPIView,SpectacularSwaggerView
 
 urlpatterns = [
+    path('swagger/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('schema/', SpectacularJSONAPIView.as_view(), name='schema'),
     path('admin/', admin.site.urls),
     path('api/v1/users/', include('user.urls')),
     path('api/v1/users/password_reset/', include('django_rest_passwordreset.urls', namespace='password_reset')),
